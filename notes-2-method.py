fd91cc91977a375fcede9359eb5b7b0c0514986a
K Luo <karinaluo267@gmail.com>
2025-10-07 10:56:10 -0700

    Added Method Notes

---
+++ notes-2-method.py
@@ -0,0 +1,13 @@
+# (String) Methods
+# Karina Luo
+# 6 Oct
+# Ask the user what the weathr is like
+# weather = input("What's the weather like today?")
+
+# if weather.lower().strip(". !?,<>@:") == "rainy":
+#     print("You should bring an umberlla.")
+# else:
+#     print("I see...")
+
+n = input()
+print(n.strip())
