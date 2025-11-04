e7767dfc6b12493c46135ba1f2099f5f45b59534
K Luo <karinaluo267@gmail.com>
2025-11-04 10:35:12 -0800

    notes 3 added

---
+++ notes-3-functions.py
@@ -0,0 +1,19 @@
+# Functions
+# Author: Karina
+# Oct 8
+
+# function to print "Hello" to the console
+def say_hello():
+    print("Hello")
+
+
+say_hello()
+
+# function to print a personalize hello
+
+
+def say_hello_personalized(name: str):
+    print(f"Hello {name}")
+
+
+say_hello_personalized("Kayi")
